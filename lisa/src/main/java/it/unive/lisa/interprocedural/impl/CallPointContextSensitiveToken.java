63da975e1e4c4907b6ee9f0426644a72c0c3c6c3
Pietro Ferrara <pietro.ferrara@unive.it>
2021-02-18 18:26:53 +0100

    #41 adding call point sensitive token

---
+++ lisa/src/main/java/it/unive/lisa/interprocedural/impl/CallPointContextSensitiveToken.java
@@ -0,0 +1,37 @@
+package it.unive.lisa.interprocedural.impl;
+
+import it.unive.lisa.program.cfg.statement.Call;
+
+import java.util.Objects;
+
+public class CallPointContextSensitiveToken extends ContextSensitiveToken {
+    private final Call callPoint;
+
+    private CallPointContextSensitiveToken(Call callPoint) {
+        this.callPoint = callPoint;
+    }
+
+    @Override
+    public ContextSensitiveToken empty() {
+        return new CallPointContextSensitiveToken(null);
+    }
+
+    @Override
+    public ContextSensitiveToken pushCall(Call c) {
+        return new CallPointContextSensitiveToken(c);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CallPointContextSensitiveToken that = (CallPointContextSensitiveToken) o;
+        return Objects.equals(callPoint, that.callPoint);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(callPoint);
+    }
+
+}
