86dee22e05221fff4cc0de76edf744ceba913789
lucaneg <luca.negrinii@gmail.com>
2021-03-25 12:31:01 +0100

    ReachingDefinitions, AvailableExpressions and ConstantPropagation

---
+++ lisa/src/test/java/it/unive/lisa/cron/dataflow/ReachingDefinitionsTest.java
@@ -0,0 +1,22 @@
+package it.unive.lisa.cron.dataflow;
+
+import static it.unive.lisa.LiSAFactory.getDefaultFor;
+
+import org.junit.Test;
+
+import it.unive.lisa.AnalysisSetupException;
+import it.unive.lisa.AnalysisTestExecutor;
+import it.unive.lisa.LiSAConfiguration;
+import it.unive.lisa.analysis.AbstractState;
+import it.unive.lisa.analysis.dataflow.impl.ReachingDefinitions;
+import it.unive.lisa.analysis.heap.HeapDomain;
+
+public class ReachingDefinitionsTest extends AnalysisTestExecutor {
+
+	@Test
+	public void testReachingDefinitions() throws AnalysisSetupException {
+		LiSAConfiguration conf = new LiSAConfiguration().setDumpAnalysis(true).setAbstractState(
+				getDefaultFor(AbstractState.class, getDefaultFor(HeapDomain.class), new ReachingDefinitions()));
+		perform("reaching-definitions", "reaching-definitions.imp", conf);
+	}
+}
