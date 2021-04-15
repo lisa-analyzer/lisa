d5687af4127b50a60db0691da1e26909b6c5b157
VincenzoArceri <vincenzoarceri.92@gmail.com>
2021-04-15 17:06:45 +0200

    #63 javadoc

--- lisa/src/main/java/it/unive/lisa/analysis/lattices/ExpressionSet.java
+++ lisa/src/main/java/it/unive/lisa/analysis/lattices/ExpressionSet.java
@@ -5,16 +5,36 @@ import it.unive.lisa.symbolic.value.Skip;
 import java.util.Collections;
 import java.util.Set;
 
+/**
+ * A set lattice containing a set of symbolic expressions.
+ * 
+ * @author <a href="mailto:vincenzo.arceri@unive.it">Vincenzo Arceri</a>
+ *
+ * @param <T> the type of the tracked symbolic expressions
+ */
 public class ExpressionSet<T extends SymbolicExpression> extends SetLattice<ExpressionSet<T>, T> {
 
+	/**
+	 * Builds the empty set lattice element.
+	 */
 	public ExpressionSet() {
 		this(Collections.emptySet());
 	}
 
+	/**
+	 * Builds a singleton set lattice element.
+	 * 
+	 * @param exp the expression
+	 */
 	public ExpressionSet(T exp) {
 		this(Collections.singleton(exp));
 	}
 
+	/**
+	 * Builds a set lattice element.
+	 * 
+	 * @param set the set of expression
+	 */
 	public ExpressionSet(Set<T> set) {
 		super(set);
 	}
