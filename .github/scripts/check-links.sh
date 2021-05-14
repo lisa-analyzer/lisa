af24d84654c6aeb3ab48c104eba61e49da876fdf
lucaneg <luca.negrinii@gmail.com>
2021-05-14 11:09:33 +0200

    #96 add workflow for checking broken links in docs

---
+++ .github/scripts/check-links.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+declare -a whitelist=(
+	"https://www.sciencedirect.com/science/article/pii/S0304397516300299"
+	)
+
+function check_link() {
+	# we remove any in-page anchor
+	polished=$(echo $link | cut -d'#' -f 1)
+	if [ -z "$polished" ]; then
+		# in-page link
+		return 0
+	fi
+	
+	for item in "${whitelist[@]}"; do
+		if [[ "$polished" == "$item" ]]; then
+			echo "- whitelisted: $polished"
+			return 0
+		fi
+	done
+
+	if [[ $polished == https://* ]]; then
+		curl -o /dev/null -Ifs "$polished"
+		if [[ $? -ne 0 ]]; then
+			echo "- broken web link: $polished"
+			return 1
+		fi
+	else
+		if [ ! -f "$(dirname $1)/$polished" ]; then
+			echo "- broken file ref: $polished"
+			return 1
+		fi
+	fi
+	return 0
+}
+
+readmes=$(find $1 -type f -regex "[^_]*\.md")
+code=0
+for file in ${readmes[@]}; do
+	echo Checking $file
+	hreflinks=$(grep -oP '(?<=href=").*?(?=")' $file)
+	varlinks=$(grep -oP '(?<=\]:).*?(?=$)' $file)
+	inlinelinks=$(grep -oP '(?<=\]\().*?(?=\))' $file)
+	
+	for link in ${hreflinks}; do
+		check_link $file $link
+		let "code=code+$?"
+	done
+	
+	for link in ${varlinks}; do
+		check_link $file $link
+		let "code=code+$?"
+	done
+	
+	for link in ${inlinelinks}; do
+		check_link $file $link
+		let "code=code+$?"
+	done
+done
+
+exit $code
