23e836b96b3cc6e7de3828bcb853e82a21aef62a
lucaneg <luca.negrinii@gmail.com>
2021-06-05 17:28:33 +0200

    #102 unique implementation of the dataflow domains

--- lisa/src/main/java/it/unive/lisa/analysis/dataflow/DataflowDomain.java
+++ lisa/src/main/java/it/unive/lisa/analysis/dataflow/DataflowDomain.java
@@ -1,7 +1,19 @@
 package it.unive.lisa.analysis.dataflow;
 
+import it.unive.lisa.analysis.BaseLattice;
+import it.unive.lisa.analysis.ScopeToken;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.representation.DomainRepresentation;
+import it.unive.lisa.analysis.representation.SetRepresentation;
 import it.unive.lisa.analysis.value.ValueDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.symbolic.value.ValueExpression;
+
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
 
 /**
  * A dataflow domain that collects instances of {@link DataflowElement}. A
@@ -14,13 +26,195 @@ import java.util.Collection;
  * @param <E> the concrete type of {@link DataflowElement} contained in this
  *                domain
  */
-public interface DataflowDomain<D extends DataflowDomain<D, E>, E extends DataflowElement<D, E>>
-		extends ValueDomain<D> {
+public abstract class DataflowDomain<D extends DataflowDomain<D, E>, E extends DataflowElement<D, E>>
+		extends BaseLattice<D> implements ValueDomain<D> {
+
+	private final boolean isTop;
+
+	private final boolean isBottom;
+
+	private final Set<E> elements;
+
+	protected final E domain;
 
 	/**
-	 * Yields all the {@link DataflowElement}s contained in this domain.
+	 * Builds the domain.
 	 * 
-	 * @return the elements
+	 * @param domain   a singleton instance to be used during semantic
+	 *                     operations to perform <i>kill</i> and <i>gen</i>
+	 *                     operations
+	 * @param elements the set of elements contained in this domain
+	 * @param isTop    whether or not this domain is the top of the lattice
+	 * @param isBottom whether or not this domain is the bottom of the lattice
 	 */
-	Collection<E> getDataflowElements();
+	protected DataflowDomain(E domain, Set<E> elements, boolean isTop, boolean isBottom) {
+		this.elements = elements;
+		this.domain = domain;
+		this.isTop = isTop;
+		this.isBottom = isBottom;
+	}
+
+	protected abstract D mk(E domain, Set<E> elements, boolean isTop, boolean isBottom);
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public final D assign(Identifier id, ValueExpression expression, ProgramPoint pp) throws SemanticException {
+		if (isBottom())
+			return (D) this;
+
+		// if id cannot be tracked by the underlying lattice,
+		// or if the expression cannot be processed, return this
+		if (!domain.tracksIdentifiers(id) || !domain.canProcess(expression))
+			return (D) this;
+		D killed = forgetIdentifiers(domain.kill(id, expression, pp, (D) this));
+		Set<E> updated = new HashSet<>(killed.getDataflowElements());
+		for (E generated : domain.gen(id, expression, pp, (D) this))
+			updated.add(generated);
+		return mk(domain, updated, false, false);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public final D smallStepSemantics(ValueExpression expression, ProgramPoint pp) throws SemanticException {
+		// TODO we have to implement a version of kill/gen also for semantics
+		return (D) this;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public final D assume(ValueExpression expression, ProgramPoint pp) throws SemanticException {
+		// TODO could be refined
+		return (D) this;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public final D forgetIdentifier(Identifier id) throws SemanticException {
+		if (isTop())
+			return (D) this;
+
+		Collection<E> toRemove = new LinkedList<>();
+		for (E e : elements)
+			if (e.getIdentifier().equals(id))
+				toRemove.add(e);
+
+		if (toRemove.isEmpty())
+			return (D) this;
+		
+		Set<E> updated = new HashSet<>(elements);
+		updated.removeAll(toRemove);
+		return mk(domain, updated, false, false);
+	}
+
+	@Override
+	public final Satisfiability satisfies(ValueExpression expression, ProgramPoint pp) throws SemanticException {
+		// TODO could be refined
+		return Satisfiability.UNKNOWN;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((domain == null) ? 0 : domain.hashCode());
+		result = prime * result + ((elements == null) ? 0 : elements.hashCode());
+		result = prime * result + (isBottom ? 1231 : 1237);
+		result = prime * result + (isTop ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DataflowDomain<?, ?> other = (DataflowDomain<?, ?>) obj;
+		if (domain == null) {
+			if (other.domain != null)
+				return false;
+		} else if (!domain.equals(other.domain))
+			return false;
+		if (elements == null) {
+			if (other.elements != null)
+				return false;
+		} else if (!elements.equals(other.elements))
+			return false;
+		if (isBottom != other.isBottom)
+			return false;
+		if (isTop != other.isTop)
+			return false;
+		return true;
+	}
+
+	@Override
+	public final DomainRepresentation representation() {
+		return new SetRepresentation(elements, DataflowElement::representation);
+	}
+
+	@Override
+	public final D top() {
+		return mk(domain, new HashSet<>(), true, false);
+	}
+
+	@Override
+	public final boolean isTop() {
+		return elements.isEmpty() && isTop;
+	}
+
+	@Override
+	public final D bottom() {
+		return mk(domain, new HashSet<>(), false, true);
+	}
+
+	@Override
+	public final boolean isBottom() {
+		return elements.isEmpty() && isBottom;
+	}
+
+	public final Set<E> getDataflowElements() {
+		return elements;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public final D pushScope(ScopeToken scope) throws SemanticException {
+		if (isTop() || isBottom())
+			return (D) this;
+
+		Set<E> result = new HashSet<>();
+		E pushed;
+		for (E element : this.elements)
+			if ((pushed = element.pushScope(scope)) != null)
+				result.add(pushed);
+
+		return mk(domain, result, false, false);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public final D popScope(ScopeToken scope) throws SemanticException {
+		if (isTop() || isBottom())
+			return (D) this;
+
+		Set<E> result = new HashSet<>();
+		E popped;
+		for (E element : this.elements)
+			if ((popped = element.popScope(scope)) != null)
+				result.add(popped);
+
+		return mk(domain, result, false, false);
+	}
+
+	@Override
+	protected final D wideningAux(D other) throws SemanticException {
+		return lubAux(other);
+	}
+
+	@Override
+	public final String toString() {
+		return representation().toString();
+	}
 }
