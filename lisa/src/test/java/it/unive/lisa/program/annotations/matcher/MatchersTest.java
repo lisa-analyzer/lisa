8ecd445ffdda4f84de8dd96956750761539ac7c3
lucaneg <luca.negrinii@gmail.com>
2021-06-05 16:49:44 +0200

    #98 testing annotation matchers

---
+++ lisa/src/test/java/it/unive/lisa/program/annotations/matcher/MatchersTest.java
@@ -0,0 +1,30 @@
+package it.unive.lisa.program.annotations.matcher;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import it.unive.lisa.program.annotations.Annotation;
+import it.unive.lisa.program.annotations.AnnotationMember;
+import it.unive.lisa.program.annotations.Annotations;
+import it.unive.lisa.program.annotations.values.IntAnnotationValue;
+
+public class MatchersTest {
+
+	@Test
+	public void testBasicMatcher() {
+		BasicAnnotationMatcher matcher = new BasicAnnotationMatcher("foo");
+		assertFalse(new Annotations().contains(matcher));
+		assertTrue(new Annotations(new Annotation("foo")).contains(matcher));
+		assertFalse(new Annotations(new Annotation("bar")).contains(matcher));
+		assertTrue(new Annotations(new Annotation("bar"), new Annotation("foo")).contains(matcher));
+		assertTrue(new Annotations(new Annotation("foo"), new Annotation("bar")).contains(matcher));
+		assertTrue(new Annotations(new Annotation("foo"), new Annotation("foo")).contains(matcher));
+
+		Annotation ann = new Annotation("foo", List.of(new AnnotationMember("f", new IntAnnotationValue(5))));
+		assertTrue(new Annotations(new Annotation("bar"), ann).contains(matcher));
+	}
+}
