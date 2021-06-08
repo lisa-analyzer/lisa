cc56105ff964c2afb1e04ddfcf19346bb707070e
VincenzoArceri <vincenzoarceri.92@gmail.com>
2021-06-08 14:03:58 +0200

    #95 simple test on context sensitive analysis with point-based heap analysis

--- lisa/src/test/java/it/unive/lisa/cron/interprocedural/ContextSensitiveAnalysisTest.java
+++ lisa/src/test/java/it/unive/lisa/cron/interprocedural/ContextSensitiveAnalysisTest.java
@@ -2,16 +2,19 @@ package it.unive.lisa.cron.interprocedural;
 
 import static it.unive.lisa.LiSAFactory.getDefaultFor;
 
+import org.junit.Test;
+
 import it.unive.lisa.AnalysisSetupException;
 import it.unive.lisa.AnalysisTestExecutor;
 import it.unive.lisa.LiSAConfiguration;
 import it.unive.lisa.analysis.AbstractState;
 import it.unive.lisa.analysis.heap.HeapDomain;
+import it.unive.lisa.analysis.impl.heap.pointbased.PointBasedHeap;
+import it.unive.lisa.analysis.impl.numeric.Interval;
 import it.unive.lisa.analysis.impl.numeric.Sign;
 import it.unive.lisa.interprocedural.callgraph.impl.RTACallGraph;
 import it.unive.lisa.interprocedural.impl.ContextBasedAnalysis;
 import it.unive.lisa.interprocedural.impl.RecursionFreeToken;
-import org.junit.Test;
 
 public class ContextSensitiveAnalysisTest extends AnalysisTestExecutor {
 
@@ -53,6 +56,16 @@ public class ContextSensitiveAnalysisTest extends AnalysisTestExecutor {
 				.setInterproceduralAnalysis(new ContextBasedAnalysis<>(RecursionFreeToken.getSingleton()))
 				.setCallGraph(new RTACallGraph());
 		perform("interprocedural", "RTAContextSensitive4", "programContextSensitive3.imp", conf);
+	}
+	
+	@Test
+	public void testRTAContextSensitive5() throws AnalysisSetupException {
+		LiSAConfiguration conf = new LiSAConfiguration().setInferTypes(true)
+				.setAbstractState(getDefaultFor(AbstractState.class,  new PointBasedHeap(), new Interval()))
+				.setDumpAnalysis(true)
+				.setInterproceduralAnalysis(new ContextBasedAnalysis<>())
+				.setCallGraph(new RTACallGraph());
+		perform("interprocedural", "RTAContextSensitive5", "programContextSensitive4.imp", conf);
 	}
 
 }
