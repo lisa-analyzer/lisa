51aa5ea063d76267094366f1491d541c5001ac79
lucaneg <luca.negrinii@gmail.com>
2021-09-08 15:36:53 +0200

    junit deps + minor test adjustments

--- lisa-imp/src/test/java/it/unive/lisa/imp/IMPFrontendTest.java
+++ lisa-imp/src/test/java/it/unive/lisa/imp/IMPFrontendTest.java
@@ -4,15 +4,12 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-import it.unive.lisa.imp.IMPFrontend;
-import it.unive.lisa.imp.ParsingException;
-
 public class IMPFrontendTest {
 
 	@Test
 	public void testExampleProgram() {
 		try {
-			IMPFrontend.processFile("imp-testcases/example.imp", false);
+			IMPFrontend.processFile("example.imp", false);
 		} catch (ParsingException e) {
 			fail("Processing the example file thrown an exception: " + e);
 		}
