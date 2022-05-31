31d22ef32515d736d34ceab12727d72098cee545
Simone Leoni <leonisimone@protonmail.com>
2022-05-31 10:01:43 +0200

    Added javadoc.

--- lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Transition.java
+++ lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Transition.java
@@ -2,6 +2,12 @@ package it.unive.lisa.analysis.string;
 
 import java.util.Objects;
 
+/**
+ * A class that describes an Automaton transition.
+ * 
+ * @author <a href="mailto:simone.leoni2@studenti.unipr.it">Simone Leoni</a>
+ * @author <a href="mailto:vincenzo.arceri@unive.it">Vincenzo Arceri</a>
+ */
 public final class Transition {
 
 	private final State source, destination;
@@ -9,6 +15,12 @@ public final class Transition {
 	// if symbol is ' ' means it is an epsilon transition
 	private final char symbol;
 
+	/**
+	 * Creates a new transition for a generic automaton.
+	 * @param source state from which the transition could be applied.
+	 * @param destination state reached after the transition execution.
+	 * @param symbol the character that have to be read to execute the transition. {@code ' '} is used for epsilon transitions.
+	 */
 	public Transition(State source, State destination, char symbol) {
 		this.source = source;
 		this.destination = destination;
@@ -32,15 +44,27 @@ public final class Transition {
 		return Objects.equals(destination, other.destination) && Objects.equals(source, other.source)
 				&& symbol == other.symbol;
 	}
-
+	
+	/**
+	 * Tells transition's source state.
+	 * @return source of {@code this} transition.
+	 */
 	public State getSource() {
 		return this.source;
 	}
 
+	/**
+	 * Tells input symbol for {@code this} transition.
+	 * @return symbol needed to execute {@code this} transition.
+	 */
 	public char getSymbol() {
 		return this.symbol;
 	}
 
+	/**
+	 * Tells transition's destination state.
+	 * @return destination of {@code this} transition.
+	 */
 	public State getDestination() {
 		return this.destination;
 	}
