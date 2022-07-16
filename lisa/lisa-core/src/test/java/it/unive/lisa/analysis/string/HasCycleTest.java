6c715ea4902adc44ae4827e99d0959ba78e735c2
sioel0 <leonisimone@protonmail.com>
2022-07-16 19:20:11 +0200

    Added HasCycleTest

---
+++ lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/HasCycleTest.java
@@ -0,0 +1,48 @@
+package it.unive.lisa.analysis.string;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class HasCycleTest {
+
+	@Test
+	public void test01() {
+		Set<State> states = new HashSet<>();
+		State[] st = new State[2];
+		st[0] = new State(true, false);
+		st[1] = new State(false, true);
+
+		states.add(st[0]);
+		states.add(st[1]);
+
+		Set<Transition> transitions = new HashSet<>();
+		transitions.add(new Transition(st[0], st[0], "a"));
+		transitions.add(new Transition(st[0], st[0], "b"));
+		transitions.add(new Transition(st[0], st[1], "b"));
+
+		// accepts language {a^nb^m}^p
+		Automaton nfa = new Automaton(states, transitions);
+		assertTrue(nfa.hasCycle());
+	}
+
+	@Test
+	public void test02() {
+		Set<State> st = new HashSet<>();
+		Set<Transition> tr = new HashSet<>();
+		State q0 = new State(true, false);
+		State q1 = new State(false, false);
+		State q2 = new State(false, true);
+		st.add(q0);
+		st.add(q1);
+		st.add(q2);
+		tr.add(new Transition(q0, q1, "a"));
+		tr.add(new Transition(q1, q2, "b"));
+		Automaton a = new Automaton(st, tr);
+		assertFalse(a.hasCycle());
+	}
+}
