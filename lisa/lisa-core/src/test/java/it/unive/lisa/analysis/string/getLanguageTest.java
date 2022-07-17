c212bbb8872f1da6d2edd40d7443d6fd31316897
sioel0 <leonisimone@protonmail.com>
2022-07-17 15:05:41 +0200

    Added more tests for getLanguage

--- lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/getLanguageTest.java
+++ lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/getLanguageTest.java
@@ -3,6 +3,7 @@ package it.unive.lisa.analysis.string;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -44,5 +45,116 @@ public class getLanguageTest {
 		// accepts language {a^nb^m}^p
 		Automaton nfa = new Automaton(states, transitions);
 		assertEquals(nfa.getLanguage(), new HashSet<String>());
+	}
+
+	@Test
+	public void test03() {
+		Set<State> states = new HashSet<>();
+		State[] st = new State[6];
+		st[0] = new State(true, false);
+		st[1] = new State(false, false);
+		st[2] = new State(false, false);
+		st[3] = new State(false, false);
+		st[4] = new State(false, true);
+		st[5] = new State(false, true);
+		Collections.addAll(states, st);
+
+		Set<Transition> delta = new HashSet<>();
+		delta.add(new Transition(st[0], st[1], "a"));
+		delta.add(new Transition(st[0], st[2], "b"));
+		delta.add(new Transition(st[1], st[3], "c"));
+		delta.add(new Transition(st[2], st[4], "d"));
+		delta.add(new Transition(st[3], st[5], "d"));
+
+		Automaton a = new Automaton(states, delta);
+		Set<String> expected = new HashSet<>();
+		expected.add("acd");
+		expected.add("bd");
+
+		assertEquals(expected, a.getLanguage());
+	}
+
+	@Test
+	public void test04() {
+		Set<State> states = new HashSet<>();
+		State[] st = new State[5];
+		st[0] = new State(true, false);
+		st[1] = new State(false, false);
+		st[2] = new State(false, false);
+		st[3] = new State(false, true);
+		st[4] = new State(false, true);
+
+		Collections.addAll(states, st);
+		Set<Transition> delta = new HashSet<>();
+		delta.add(new Transition(st[0], st[1], ""));
+		delta.add(new Transition(st[0], st[2], ""));
+		delta.add(new Transition(st[1], st[3], "a"));
+		delta.add(new Transition(st[2], st[4], "b"));
+
+		Set<String> exp = new HashSet<>();
+		exp.add("a");
+		exp.add("b");
+
+		Automaton a = new Automaton(states, delta);
+
+		assertEquals(exp, a.getLanguage());
+	}
+
+	@Test
+	public void test05() {
+		Set<State> states = new HashSet<>();
+		State[] st = new State[5];
+		st[0] = new State(true, false);
+		st[1] = new State(false, false);
+		st[2] = new State(false, false);
+		st[3] = new State(false, false);
+		st[4] = new State(false, true);
+		Collections.addAll(states, st);
+
+		Set<Transition> delta = new HashSet<>();
+		delta.add(new Transition(st[0], st[1], "a"));
+		delta.add(new Transition(st[1], st[2], "b"));
+		delta.add(new Transition(st[1], st[3], "a"));
+		delta.add(new Transition(st[3], st[4], "c"));
+		delta.add(new Transition(st[2], st[4], "c"));
+
+		Set<String> exp = new HashSet<>();
+		exp.add("abc");
+		exp.add("aac");
+
+		Automaton a = new Automaton(states, delta);
+		assertEquals(a.getLanguage(), exp);
+	}
+
+	@Test
+	public void test06() {
+		Set<State> states = new HashSet<>();
+		State[] st = new State[8];
+		st[0] = new State(true, false);
+		st[1] = new State(false, false);
+		st[2] = new State(false, false);
+		st[3] = new State(false, false);
+		st[4] = new State(false, false);
+		st[5] = new State(false, false);
+		st[6] = new State(false, false);
+		st[7] = new State(false, true);
+		Collections.addAll(states, st);
+
+		Set<Transition> delta = new HashSet<>();
+		delta.add(new Transition(st[0], st[1], "a"));
+		delta.add(new Transition(st[1], st[2], ""));
+		delta.add(new Transition(st[2], st[3], "b"));
+		delta.add(new Transition(st[2], st[4], "c"));
+		delta.add(new Transition(st[3], st[5], ""));
+		delta.add(new Transition(st[4], st[6], "d"));
+		delta.add(new Transition(st[5], st[7], "c"));
+		delta.add(new Transition(st[6], st[7], "c"));
+
+		Set<String> exp = new HashSet<>();
+		exp.add("abc");
+		exp.add("acdc");
+
+		Automaton a = new Automaton(states, delta);
+		assertEquals(a.getLanguage(), exp);
 	}
 }
