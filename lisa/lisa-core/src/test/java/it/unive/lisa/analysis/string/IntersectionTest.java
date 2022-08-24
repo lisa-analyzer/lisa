949b4df43b647b0279aff3411fe9904013ce84f7
sioel0 <leonisimone@protonmail.com>
2022-08-24 16:36:17 +0200

    Added new test for Intersection function

--- lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/IntersectionTest.java
+++ lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/IntersectionTest.java
@@ -18,6 +18,7 @@ public class IntersectionTest {
         State[] st2 = new State[3];
         Set<Transition> delta = new HashSet<>();
         Set<Transition> delta2 = new HashSet<>();
+
         st[0] = new State(true, false);
         st[1] = new State(false, false);
         st[2] = new State(false, false);
@@ -42,6 +43,40 @@ public class IntersectionTest {
         Automaton a = new Automaton(states, delta);
 
         // ab
+        Automaton a2 = new Automaton(states2, delta2);
+
+        assertTrue(a.intersection(a2).isEqual(a2));
+    }
+
+    @Test
+    public void test02() {
+        Set<State> states = new HashSet<>();
+        State[] st = new State[4];
+        Set<State> states2 = new HashSet<>();
+        State[] st2 = new State[3];
+        Set<Transition> delta = new HashSet<>();
+        Set<Transition> delta2 = new HashSet<>();
+
+        st[0] = new State(true, false);
+        st[1] = new State(false, false);
+        st[2] = new State(false, true);
+        st[3] = new State(false, true);
+        Collections.addAll(states, st);
+
+        delta.add(new Transition(st[0], st[1], "a"));
+        delta.add(new Transition(st[1], st[2], "c"));
+        delta.add(new Transition(st[2], st[3], "b"));
+
+        st2[0] = new State(true, false);
+        st2[1] = new State(false, false);
+        st2[2] = new State(false, true);
+        Collections.addAll(states2, st2);
+
+        delta2.add(new Transition(st2[0], st2[1], "a"));
+        delta2.add(new Transition(st2[1], st2[2], "c"));
+
+        Automaton a = new Automaton(states, delta);
+
         Automaton a2 = new Automaton(states2, delta2);
 
         assertTrue(a.intersection(a2).isEqual(a2));
