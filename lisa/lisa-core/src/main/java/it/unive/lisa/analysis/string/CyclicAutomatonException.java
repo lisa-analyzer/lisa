0541c7b8082a45e1dcadbf9093d70811bc06e275
sioel0 <leonisimone@protonmail.com>
2022-08-26 23:02:36 +0200

    Added javadoc, fixed toRegex and added some more test for toRegex

--- lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/CyclicAutomatonException.java
+++ lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/CyclicAutomatonException.java
@@ -1,6 +1,12 @@
 package it.unive.lisa.analysis.string;
 
+/**
+ * Exception thrown if an automaton is cyclic while computing accepted language.
+ */
 public class CyclicAutomatonException extends Exception {
+    /**
+     * Default constructor for CyclicAutomatonException
+     */
     public CyclicAutomatonException() {
         super("The automaton is cyclic");
     }
--- lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/ToRegexTest.java
+++ lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/ToRegexTest.java
@@ -3,7 +3,7 @@ package it.unive.lisa.analysis.string;
 import org.junit.Test;
 
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
@@ -12,28 +12,28 @@ public class ToRegexTest {
 
     @Test
     public void test01() {
-        Set<State> states = new HashSet<>();
+        Set<State> states = new LinkedHashSet<>();
         State[] st = new State[1];
         st[0] = new State(true, true);
         Collections.addAll(states, st);
 
-        Set<Transition> delta = new HashSet<>();
+        Set<Transition> delta = new LinkedHashSet<>();
         delta.add(new Transition(st[0], st[0], "a"));
 
         Automaton a = new Automaton(states, delta);
 
-        assertEquals("(a)*", a.toRegex());
+        assertEquals("a*", a.toRegex());
     }
 
     @Test
     public void test02() {
-        Set<State> states = new HashSet<>();
+        Set<State> states = new LinkedHashSet<>();
         State[] st = new State[2];
         st[0] = new State(true, false);
         st[1] = new State(false, true);
         Collections.addAll(states, st);
 
-        Set<Transition> delta = new HashSet<>();
+        Set<Transition> delta = new LinkedHashSet<>();
         delta.add(new Transition(st[0], st[0], "c"));
         delta.add(new Transition(st[0], st[1], "a"));
         delta.add(new Transition(st[1], st[0], "b"));
@@ -41,12 +41,12 @@ public class ToRegexTest {
 
         Automaton a = new Automaton(states, delta);
 
-        assertEquals("((c)*(a))(d|(b)(c)*(a))*", a.toRegex());
+        assertEquals("c*a(d|bc*a)*", a.toRegex());
     }
 
     @Test
     public void test03() {
-        Set<State> states = new HashSet<>();
+        Set<State> states = new LinkedHashSet<>();
         State[] st = new State[3];
 
         st[0] = new State(true, false);
@@ -54,37 +54,37 @@ public class ToRegexTest {
         st[2] = new State(false, true);
         Collections.addAll(states, st);
 
-        Set<Transition> delta = new HashSet<>();
+        Set<Transition> delta = new LinkedHashSet<>();
         delta.add(new Transition(st[0], st[1], "a"));
         delta.add(new Transition(st[0], st[2], "c"));
 
         // ab | cc
         Automaton a = new Automaton(states, delta);
-        assertEquals("((c)|(a))", a.toRegex());
+        assertEquals("c|a", a.toRegex());
     }
 
     @Test
     public void test04() {
-        Set<State> states = new HashSet<>();
+        Set<State> states = new LinkedHashSet<>();
         State[] st = new State[3];
         st[0] = new State(true, false);
         st[1] = new State(false, false);
         st[2] = new State(false, true);
         Collections.addAll(states, st);
 
-        Set<Transition> delta = new HashSet<>();
+        Set<Transition> delta = new LinkedHashSet<>();
         delta.add(new Transition(st[0], st[1], "a"));
         delta.add(new Transition(st[1], st[2], "b"));
 
         Automaton a = new Automaton(states, delta);
-        assertEquals("(a)(b)", a.toRegex());
+        assertEquals("ab", a.toRegex());
     }
 
     @Test
     public void test05() {
-        Set<State> states = new HashSet<>();
+        Set<State> states = new LinkedHashSet<>();
         State[] st = new State[5];
-        Set<Transition> delta = new HashSet<>();
+        Set<Transition> delta = new LinkedHashSet<>();
 
         st[0] = new State(true, false);
         st[1] = new State(false, false);
@@ -102,5 +102,47 @@ public class ToRegexTest {
         Automaton a = new Automaton(states, delta);
 
         assertEquals("ab|cc", a.toRegex());
+    }
+
+    @Test
+    public void test06() {
+        Set<State> states = new LinkedHashSet<>();
+        State[] st = new State[4];
+        st[0] = new State(true, false);
+        st[1] = new State(false, false);
+        st[2] = new State(false, true);
+        st[3] = new State(false, true);
+        Collections.addAll(states, st);
+
+        Set<Transition> delta = new LinkedHashSet<>();
+        delta.add(new Transition(st[0], st[1], ""));
+        delta.add(new Transition(st[1], st[2], "a"));
+        delta.add(new Transition(st[1], st[3], "b"));
+        delta.add(new Transition(st[2], st[2], "c"));
+        delta.add(new Transition(st[3], st[3], "c"));
+
+        Automaton a = new Automaton(states, delta);
+
+        assertEquals("ac*|bc*", a.toRegex());
+    }
+
+    @Test
+    public void test07() {
+        Set<State> states = new LinkedHashSet<>();
+        State[] st = new State[4];
+        st[0] = new State(true, false);
+        st[1] = new State(false, false);
+        st[2] = new State(false, false);
+        st[3] = new State(false, true);
+        Collections.addAll(states, st);
+
+        Set<Transition> delta = new LinkedHashSet<>();
+        delta.add(new Transition(st[0], st[1], "a"));
+        delta.add(new Transition(st[0], st[2], "a"));
+        delta.add(new Transition(st[1], st[3], "b"));
+        delta.add(new Transition(st[2], st[3], "c"));
+
+        Automaton a = new Automaton(states, delta);
+        assertEquals("a(c|b)", a.toRegex());
     }
 }
