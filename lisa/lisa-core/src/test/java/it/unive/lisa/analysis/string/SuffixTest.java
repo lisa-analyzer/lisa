4508114ed0b22851c323e06efb3622f725829588
Sergio Evola <Evola.sergio97@gmail.com>
2022-11-02 15:14:19 +0100

    add testIsTop() and testIsBottom() methods, refactoring

--- lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/SuffixTest.java
+++ lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/SuffixTest.java
@@ -8,47 +8,56 @@ import static org.junit.Assert.*;
 public class SuffixTest {
 
     @Test
-    public void testSuffixConstructor(){
+    public void testConstructor(){
         new Suffix();
     }
 
     @Test
-    public void testSuffixConstructor1(){
+    public void testConstructor1(){
         new Suffix("Hello World!");
     }
 
     @Test
-    public void testSuffixLubAux() throws SemanticException {
+    public void testLubAux() throws SemanticException {
         Suffix result = new Suffix("Hello World!").lubAux(new Suffix("World!"));
 
         assertEquals(result.getSuffix(), "World!");
     }
 
     @Test
-    public void testSuffixLubAux1() throws SemanticException {
+    public void testLubAux1() throws SemanticException {
         Suffix result = new Suffix("abcde").lubAux(new Suffix("cde"));
 
         assertEquals(result.getSuffix(), "cde");
     }
 
     @Test
-    public void testSuffixLubAux2() throws SemanticException {
+    public void testLubAux2() throws SemanticException {
         Suffix result = new Suffix("Hello").lubAux(new Suffix("World"));
 
         assertTrue(result.isTop());
     }
 
     @Test
-    public void testSuffixLessOrEqual() throws SemanticException {
+    public void testLessOrEqual() throws SemanticException {
         Suffix suffix = new Suffix("fghabc");
 
         assertTrue(suffix.lessOrEqualAux(new Suffix("abc")));
     }
 
     @Test
-    public void testSuffixLessOrEqual1() throws SemanticException {
+    public void testLessOrEqual1() throws SemanticException {
         Suffix suffix = new Suffix("fghabc");
 
         assertFalse(suffix.lessOrEqualAux(new Suffix("abd")));
+    }
+    @Test
+    public void testIsTop(){
+        assertTrue(new Suffix("").isTop());
+    }
+
+    @Test
+    public void testIsBottom(){
+        assertTrue(new Suffix(null).isBottom());
     }
 }
