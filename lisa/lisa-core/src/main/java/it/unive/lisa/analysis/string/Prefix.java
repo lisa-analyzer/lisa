e384dbc4aa5c45151a316e41192c37ae28417287
Sergio Evola <Evola.sergio97@gmail.com>
2022-11-09 14:41:12 +0100

    replace javadoc link from plain text to html

--- lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Prefix.java
+++ lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Prefix.java
@@ -15,9 +15,11 @@ import java.util.Objects;
 
 /**
  * The prefix string abstract domain.
- * 
- * @link https://link.springer.com/chapter/10.1007/978-3-642-24559-6_34
- * 
+ *
+ * @link <a href=
+ *           "https://link.springer.com/chapter/10.1007/978-3-642-24559-6_34">
+ *           https://link.springer.com/chapter/10.1007/978-3-642-24559-6_34</a>
+ *
  * @author <a href="mailto:vincenzo.arceri@unipr.it">Vincenzo Arceri</a>
  * @author <a href="mailto:sergiosalvatore.evola@studenti.unipr.it">Sergio
  *             Salvatore Evola</a>
