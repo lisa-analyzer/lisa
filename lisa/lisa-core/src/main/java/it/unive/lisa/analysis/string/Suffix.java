203ef266db2d217abdc6f1dc59f6f36de6992cd8
Sergio Evola <Evola.sergio97@gmail.com>
2022-11-09 12:53:08 +0100

    run spotless apply

--- lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Suffix.java
+++ lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Suffix.java
@@ -11,159 +11,163 @@ import it.unive.lisa.symbolic.value.Constant;
 import it.unive.lisa.symbolic.value.operator.binary.*;
 import it.unive.lisa.symbolic.value.operator.ternary.TernaryOperator;
 import it.unive.lisa.symbolic.value.operator.unary.UnaryOperator;
-
 import java.util.Objects;
 
 public class Suffix extends BaseNonRelationalValueDomain<Suffix> {
 
-    private final static Suffix TOP = new Suffix();
-    private final static Suffix BOTTOM = new Suffix(null);
-    private final String suffix;
+	private final static Suffix TOP = new Suffix();
+	private final static Suffix BOTTOM = new Suffix(null);
+	private final String suffix;
 
-    public Suffix() {
-        this("");
-    }
+	public Suffix() {
+		this("");
+	}
 
-    public Suffix(String suffix) {
-        this.suffix = suffix;
-    }
+	public Suffix(String suffix) {
+		this.suffix = suffix;
+	}
 
-    @Override
-    public Suffix lubAux(Suffix other) throws SemanticException {
-        String otherSuffix = other.getSuffix();
-        StringBuilder result = new StringBuilder();
+	@Override
+	public Suffix lubAux(Suffix other) throws SemanticException {
+		String otherSuffix = other.getSuffix();
+		StringBuilder result = new StringBuilder();
 
-        int i = suffix.length() - 1;
-        int j = otherSuffix.length() - 1;
+		int i = suffix.length() - 1;
+		int j = otherSuffix.length() - 1;
 
-        while (i >= 0 && j >= 0 &&
-                suffix.charAt(i) == otherSuffix.charAt(j)) {
-            result.append(suffix.charAt(i--));
-            j--;
-        }
+		while (i >= 0 && j >= 0 &&
+				suffix.charAt(i) == otherSuffix.charAt(j)) {
+			result.append(suffix.charAt(i--));
+			j--;
+		}
 
-        if (result.length() != 0)
-            return new Suffix(result.reverse().toString());
+		if (result.length() != 0)
+			return new Suffix(result.reverse().toString());
 
-        else
-            return TOP;
-    }
+		else
+			return TOP;
+	}
 
-    @Override
-    public Suffix wideningAux(Suffix other) throws SemanticException {
-        return lubAux(other);
-    }
+	@Override
+	public Suffix wideningAux(Suffix other) throws SemanticException {
+		return lubAux(other);
+	}
 
-    @Override
-    public boolean lessOrEqualAux(Suffix other) throws SemanticException {
-        if (other.getSuffix().length() <= this.getSuffix().length()) {
-            Suffix lub = this.lubAux(other);
-            String lubString = lub.getSuffix();
+	@Override
+	public boolean lessOrEqualAux(Suffix other) throws SemanticException {
+		if (other.getSuffix().length() <= this.getSuffix().length()) {
+			Suffix lub = this.lubAux(other);
+			String lubString = lub.getSuffix();
 
-            return lubString.length() == other.getSuffix().length();
-        }
+			return lubString.length() == other.getSuffix().length();
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Suffix suffix1 = (Suffix) o;
-        return Objects.equals(suffix, suffix1.suffix);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Suffix suffix1 = (Suffix) o;
+		return Objects.equals(suffix, suffix1.suffix);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(suffix);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(suffix);
+	}
 
-    @Override
-    public Suffix top() {
-        return TOP;
-    }
+	@Override
+	public Suffix top() {
+		return TOP;
+	}
 
-    @Override
-    public Suffix bottom() {
-        return BOTTOM;
-    }
+	@Override
+	public Suffix bottom() {
+		return BOTTOM;
+	}
 
-    @Override
-    public boolean isTop() {
-        return this.equals(TOP);
-    }
+	@Override
+	public boolean isTop() {
+		return this.equals(TOP);
+	}
 
-    @Override
-    public boolean isBottom() {
-        return this.equals(BOTTOM);
-    }
+	@Override
+	public boolean isBottom() {
+		return this.equals(BOTTOM);
+	}
 
-    @Override
-    public DomainRepresentation representation() {
-        if (isBottom())
-            return Lattice.bottomRepresentation();
-        if (isTop())
-            return Lattice.topRepresentation();
+	@Override
+	public DomainRepresentation representation() {
+		if (isBottom())
+			return Lattice.bottomRepresentation();
+		if (isTop())
+			return Lattice.topRepresentation();
 
-        return new StringRepresentation(suffix);
-    }
+		return new StringRepresentation(suffix);
+	}
 
-    @Override
-    public Suffix evalNullConstant(ProgramPoint pp) {
-        return TOP;
-    }
+	@Override
+	public Suffix evalNullConstant(ProgramPoint pp) {
+		return TOP;
+	}
 
-    @Override
-    public Suffix evalNonNullConstant(Constant constant, ProgramPoint pp) {
-        if (constant.getValue() instanceof String)
-            return new Suffix((String) constant.getValue());
+	@Override
+	public Suffix evalNonNullConstant(Constant constant, ProgramPoint pp) {
+		if (constant.getValue() instanceof String)
+			return new Suffix((String) constant.getValue());
 
-        return TOP;
-    }
+		return TOP;
+	}
 
-    @Override
-    public Suffix evalUnaryExpression(UnaryOperator operator, Suffix arg, ProgramPoint pp) {
-        return TOP;
-    }
+	@Override
+	public Suffix evalUnaryExpression(UnaryOperator operator, Suffix arg, ProgramPoint pp) {
+		return TOP;
+	}
 
-    @Override
-    public Suffix evalBinaryExpression(BinaryOperator operator, Suffix left, Suffix right, ProgramPoint pp) {
-        if (operator == StringConcat.INSTANCE) {
-            return right;
-        } else if (operator == StringContains.INSTANCE ||
-                operator == StringEndsWith.INSTANCE ||
-                operator == StringEquals.INSTANCE ||
-                operator == StringIndexOf.INSTANCE ||
-                operator == StringStartsWith.INSTANCE) {
-            return TOP;
-        }
+	@Override
+	public Suffix evalBinaryExpression(BinaryOperator operator, Suffix left, Suffix right, ProgramPoint pp) {
+		if (operator == StringConcat.INSTANCE) {
+			return right;
+		} else if (operator == StringContains.INSTANCE ||
+				operator == StringEndsWith.INSTANCE ||
+				operator == StringEquals.INSTANCE ||
+				operator == StringIndexOf.INSTANCE ||
+				operator == StringStartsWith.INSTANCE) {
+			return TOP;
+		}
 
-        return TOP;
-    }
+		return TOP;
+	}
 
-    @Override
-    public Suffix evalTernaryExpression(TernaryOperator operator, Suffix left, Suffix middle, Suffix right, ProgramPoint pp) {
-        return TOP;
-    }
+	@Override
+	public Suffix evalTernaryExpression(TernaryOperator operator, Suffix left, Suffix middle, Suffix right,
+			ProgramPoint pp) {
+		return TOP;
+	}
 
-    @Override
-    public Satisfiability satisfiesBinaryExpression(BinaryOperator operator, Suffix left, Suffix right, ProgramPoint pp) {
-        if(left.isTop() || right.isTop())
-            return Satisfiability.UNKNOWN;
+	@Override
+	public Satisfiability satisfiesBinaryExpression(BinaryOperator operator, Suffix left, Suffix right,
+			ProgramPoint pp) {
+		if (left.isTop() || right.isTop())
+			return Satisfiability.UNKNOWN;
 
-        if(operator == StringEndsWith.INSTANCE)
-            return Satisfiability.SATISFIED;
+		if (operator == StringEndsWith.INSTANCE)
+			return Satisfiability.SATISFIED;
 
-        return Satisfiability.UNKNOWN;
-    }
+		return Satisfiability.UNKNOWN;
+	}
 
-    @Override
-    public Satisfiability satisfiesTernaryExpression(TernaryOperator operator, Suffix left, Suffix middle, Suffix right, ProgramPoint pp) {
-        return Satisfiability.UNKNOWN;
-    }
+	@Override
+	public Satisfiability satisfiesTernaryExpression(TernaryOperator operator, Suffix left, Suffix middle, Suffix right,
+			ProgramPoint pp) {
+		return Satisfiability.UNKNOWN;
+	}
 
-    protected String getSuffix() {
-        return this.suffix;
-    }
+	protected String getSuffix() {
+		return this.suffix;
+	}
 }
--- lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/SuffixTest.java
+++ lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/SuffixTest.java
@@ -1,63 +1,64 @@
 package it.unive.lisa.analysis.string;
 
+import static org.junit.Assert.*;
+
 import it.unive.lisa.analysis.SemanticException;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class SuffixTest {
 
-    @Test
-    public void testConstructor(){
-        new Suffix();
-    }
+	@Test
+	public void testConstructor() {
+		new Suffix();
+	}
 
-    @Test
-    public void testConstructor1(){
-        new Suffix("Hello World!");
-    }
+	@Test
+	public void testConstructor1() {
+		new Suffix("Hello World!");
+	}
 
-    @Test
-    public void testLubAux() throws SemanticException {
-        Suffix result = new Suffix("Hello World!").lubAux(new Suffix("World!"));
+	@Test
+	public void testLubAux() throws SemanticException {
+		Suffix result = new Suffix("Hello World!").lubAux(new Suffix("World!"));
 
-        assertEquals(result.getSuffix(), "World!");
-    }
+		assertEquals(result.getSuffix(), "World!");
+	}
 
-    @Test
-    public void testLubAux1() throws SemanticException {
-        Suffix result = new Suffix("abcde").lubAux(new Suffix("cde"));
+	@Test
+	public void testLubAux1() throws SemanticException {
+		Suffix result = new Suffix("abcde").lubAux(new Suffix("cde"));
 
-        assertEquals(result.getSuffix(), "cde");
-    }
+		assertEquals(result.getSuffix(), "cde");
+	}
 
-    @Test
-    public void testLubAux2() throws SemanticException {
-        Suffix result = new Suffix("Hello").lubAux(new Suffix("World"));
+	@Test
+	public void testLubAux2() throws SemanticException {
+		Suffix result = new Suffix("Hello").lubAux(new Suffix("World"));
 
-        assertTrue(result.isTop());
-    }
+		assertTrue(result.isTop());
+	}
 
-    @Test
-    public void testLessOrEqual() throws SemanticException {
-        Suffix suffix = new Suffix("fghabc");
+	@Test
+	public void testLessOrEqual() throws SemanticException {
+		Suffix suffix = new Suffix("fghabc");
 
-        assertTrue(suffix.lessOrEqualAux(new Suffix("abc")));
-    }
+		assertTrue(suffix.lessOrEqualAux(new Suffix("abc")));
+	}
 
-    @Test
-    public void testLessOrEqual1() throws SemanticException {
-        Suffix suffix = new Suffix("fghabc");
+	@Test
+	public void testLessOrEqual1() throws SemanticException {
+		Suffix suffix = new Suffix("fghabc");
 
-        assertFalse(suffix.lessOrEqualAux(new Suffix("abd")));
-    }
-    @Test
-    public void testIsTop(){
-        assertTrue(new Suffix("").isTop());
-    }
+		assertFalse(suffix.lessOrEqualAux(new Suffix("abd")));
+	}
 
-    @Test
-    public void testIsBottom(){
-        assertTrue(new Suffix(null).isBottom());
-    }
+	@Test
+	public void testIsTop() {
+		assertTrue(new Suffix("").isTop());
+	}
+
+	@Test
+	public void testIsBottom() {
+		assertTrue(new Suffix(null).isBottom());
+	}
 }
