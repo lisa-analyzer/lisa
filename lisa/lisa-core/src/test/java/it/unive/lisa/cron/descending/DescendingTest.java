14517cb332f8bb452cc072c81f0a289a41c609e5
VincenzoArceri <vincenzoarceri.92@gmail.com>
2022-11-09 21:03:48 +0100

    Remove graph dumping in descending test

--- lisa/lisa-core/src/test/java/it/unive/lisa/cron/descending/DescendingTest.java
+++ lisa/lisa-core/src/test/java/it/unive/lisa/cron/descending/DescendingTest.java
@@ -2,16 +2,16 @@ package it.unive.lisa.cron.descending;
 
 import static it.unive.lisa.LiSAFactory.getDefaultFor;
 
+import org.junit.Test;
+
 import it.unive.lisa.AnalysisSetupException;
 import it.unive.lisa.AnalysisTestExecutor;
 import it.unive.lisa.LiSAConfiguration;
-import it.unive.lisa.LiSAConfiguration.GraphType;
 import it.unive.lisa.analysis.AbstractState;
 import it.unive.lisa.analysis.heap.HeapDomain;
 import it.unive.lisa.analysis.nonrelational.value.TypeEnvironment;
 import it.unive.lisa.analysis.numeric.Interval;
 import it.unive.lisa.analysis.types.InferredTypes;
-import org.junit.Test;
 
 public class DescendingTest extends AnalysisTestExecutor {
 
@@ -22,7 +22,6 @@ public class DescendingTest extends AnalysisTestExecutor {
 		conf.abstractState = getDefaultFor(AbstractState.class, getDefaultFor(HeapDomain.class), new Interval(),
 				new TypeEnvironment<>(new InferredTypes()));
 		conf.doDescendingPhase = true;
-		conf.analysisGraphs = GraphType.DOT;
 		perform("descending", "program.imp", conf);
 	}
 }
