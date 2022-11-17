d9e45cbe59ce00470158be60febcf19049b81a9a
Sergio Evola <Evola.sergio97@gmail.com>
2022-11-17 16:11:22 +0100

    add normalize, getReps and concatenate methods (TODO)

--- lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Brick.java
+++ lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Brick.java
@@ -1,15 +1,55 @@
 package it.unive.lisa.analysis.string;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
 
 public class Brick {
-    private final int min;
-    private final int max;
-    private final Collection<String> strings;
+	private final int min;
+	private final int max;
+	private final Collection<String> strings;
 
-    public Brick(int min, int max, Collection<String> strings){
-        this.min = min;
-        this.max = max;
-        this.strings = strings;
-    }
+	public Brick(int min, int max, Collection<String> strings) {
+		this.min = min;
+		this.max = max;
+		this.strings = strings;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Brick brick = (Brick) o;
+		return min == brick.min && max == brick.max && Objects.equals(strings, brick.strings);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(min, max, strings);
+	}
+
+	public int getMin() {
+		return min;
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+	public Collection<String> getStrings() {
+		return strings;
+	}
+
+	public Collection<String> getReps(){
+		HashSet<String> reps = new HashSet<>();
+		for(String string: this.strings){
+			StringBuilder result = new StringBuilder();
+			for(int i = 0; i < this.min; ++i){
+				result.append(string);
+			}
+		}
+		return reps;
+	}
 }
--- lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Bricks.java
+++ lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Bricks.java
@@ -4,41 +4,72 @@ import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
 import it.unive.lisa.analysis.representation.DomainRepresentation;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+
 public class Bricks extends BaseNonRelationalValueDomain<Bricks> {
 
+	private List<Brick> bricks;
 
-    @Override
-    public Bricks lubAux(Bricks other) throws SemanticException {
-        return null;
+	@Override
+	public Bricks lubAux(Bricks other) throws SemanticException {
+		return null;
+	}
+
+	@Override
+	public boolean lessOrEqualAux(Bricks other) throws SemanticException {
+		return false;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Bricks bricks1 = (Bricks) o;
+		return Objects.equals(bricks, bricks1.bricks);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bricks);
+	}
+
+	@Override
+	public Bricks top() {
+		return null;
+	}
+
+	@Override
+	public Bricks bottom() {
+		return null;
+	}
+
+	@Override
+	public DomainRepresentation representation() {
+		return null;
+	}
+
+    private Brick concatenate(Brick brick1, Brick brick2){
+		return null;
     }
 
-    @Override
-    public boolean lessOrEqualAux(Bricks other) throws SemanticException {
-        return false;
-    }
+	private void normalize() {
+        List<Brick> thisBricks = this.bricks;
+        thisBricks.removeIf(brick -> brick.getMin() == 0 &&
+				brick.getMax() == 0 &&
+				brick.getStrings().isEmpty());
 
-    @Override
-    public boolean equals(Object obj) {
-        return false;
-    }
+        for(Brick brick: thisBricks){
+            Brick nextBrick = thisBricks.get(thisBricks.indexOf(brick) + 1);
 
-    @Override
-    public int hashCode() {
-        return 0;
-    }
+            if(brick.getMin() == 1 && brick.getMax() == 1 &&
+                    nextBrick.getMin() == 1 && nextBrick.getMax() == 1){
 
-    @Override
-    public Bricks top() {
-        return null;
-    }
-
-    @Override
-    public Bricks bottom() {
-        return null;
-    }
-
-    @Override
-    public DomainRepresentation representation() {
-        return null;
-    }
+                Brick newBrick = new Brick(1,1,new HashSet<String>());
+            }
+        }
+	}
 }
