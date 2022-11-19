fd4c10bac869fb565c37497ae8e38908c7b58193
Sergio Evola <Evola.sergio97@gmail.com>
2022-11-19 19:23:04 +0100

    invert parenthesis on toString()

--- lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Brick.java
+++ lisa/lisa-core/src/main/java/it/unive/lisa/analysis/string/Brick.java
@@ -102,12 +102,12 @@ public class Brick {
 
 	@Override
 	public String toString() {
-		return "{[min: " +
+		return "[ {min: " +
 				this.min +
-				"], [max: " +
+				"}, {max: " +
 				this.max +
-				"], [strings: " +
+				"}, {strings: " +
 				StringUtils.join(this.strings, ", ") +
-				"]}";
+				"} ]";
 	}
 }
