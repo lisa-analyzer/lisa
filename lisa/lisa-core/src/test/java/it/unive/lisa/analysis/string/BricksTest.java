ad5b278b1b221fb9d6f1e64061d887e28e1acb2b
Sergio Evola <Evola.sergio97@gmail.com>
2022-11-21 16:50:49 +0100

    add normBricksRule2Test()

--- lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/BricksTest.java
+++ lisa/lisa-core/src/test/java/it/unive/lisa/analysis/string/BricksTest.java
@@ -3,6 +3,7 @@ package it.unive.lisa.analysis.string;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,16 +15,16 @@ public class BricksTest {
     public void normBricksTest(){
         List<Brick> list = new ArrayList<>();
 
-        HashSet<String> hashSet = new HashSet<>();
-        hashSet.add("a");
+        HashSet<String> strings = new HashSet<>();
+        strings.add("a");
 
-        HashSet<String> hashSet1 = new HashSet<>();
-        hashSet1.add("a");
-        hashSet1.add("b");
+        HashSet<String> strings1 = new HashSet<>();
+        strings1.add("a");
+        strings1.add("b");
 
-        list.add(new Brick(1,1,hashSet));
-        list.add(new Brick(2,3,hashSet1));
-        list.add(new Brick(0,1,hashSet1));
+        list.add(new Brick(1,1,strings));
+        list.add(new Brick(2,3,strings1));
+        list.add(new Brick(0,1,strings1));
 
         Bricks bricks = new Bricks(list);
 
@@ -31,14 +32,50 @@ public class BricksTest {
 
         List<Brick> resultList = new ArrayList<>();
 
-        HashSet<String> resultHashSet = new HashSet<>();
+        Collection<String> resultHashSet = new HashSet<>();
         resultHashSet.add("aaa");
         resultHashSet.add("aab");
         resultHashSet.add("aba");
         resultHashSet.add("abb");
 
         resultList.add(new Brick(1,1,resultHashSet));
-        resultList.add(new Brick(0,2,hashSet1));
+        resultList.add(new Brick(0,2,strings1));
+
+        Bricks resultBricks = new Bricks(resultList);
+
+        assertEquals(bricks,resultBricks);
+    }
+
+    @Test
+    public void normBricksRule2Test(){
+        List<Brick> list = new ArrayList<>();
+
+        Collection<String> strings = new HashSet<>();
+        strings.add("a");
+        strings.add("cd");
+
+        Collection<String> strings1 = new HashSet<>();
+
+        strings1.add("b");
+        strings1.add("ef");
+
+        list.add(new Brick(1,1,strings));
+        list.add(new Brick(1,1,strings1));
+
+        Bricks bricks = new Bricks(list);
+        bricks.normBricks();
+
+        Collection<String> resultStrings = new HashSet<>();
+
+        List<Brick> resultList = new ArrayList<>();
+
+
+        resultStrings.add("ab");
+        resultStrings.add("aef");
+        resultStrings.add("cdb");
+        resultStrings.add("cdef");
+
+        resultList.add(new Brick(1,1,resultStrings));
 
         Bricks resultBricks = new Bricks(resultList);
 
