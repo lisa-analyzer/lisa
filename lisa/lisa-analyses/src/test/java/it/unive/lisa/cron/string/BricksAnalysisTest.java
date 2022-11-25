fc4526f8b62ae68c5dc6747936e6eb00e3b632d7
Sergio Evola <Evola.sergio97@gmail.com>
2022-11-25 19:00:06 +0100

    add cron tests

---
+++ lisa/lisa-analyses/src/test/java/it/unive/lisa/cron/string/BricksAnalysisTest.java
@@ -0,0 +1,25 @@
+package it.unive.lisa.cron.string;
+
+import static it.unive.lisa.LiSAFactory.getDefaultFor;
+
+import it.unive.lisa.AnalysisSetupException;
+import it.unive.lisa.AnalysisTestExecutor;
+import it.unive.lisa.LiSAConfiguration;
+import it.unive.lisa.analysis.AbstractState;
+import it.unive.lisa.analysis.heap.HeapDomain;
+import it.unive.lisa.analysis.nonrelational.value.TypeEnvironment;
+import it.unive.lisa.analysis.string.Bricks;
+import it.unive.lisa.analysis.types.InferredTypes;
+import org.junit.Test;
+
+public class BricksAnalysisTest extends AnalysisTestExecutor {
+
+	@Test
+	public void testBricks() throws AnalysisSetupException {
+		LiSAConfiguration conf = new LiSAConfiguration();
+		conf.serializeResults = true;
+		conf.abstractState = getDefaultFor(AbstractState.class, getDefaultFor(HeapDomain.class), new Bricks(),
+				new TypeEnvironment<>(new InferredTypes()));
+		perform("bricks", "program.imp", conf);
+	}
+}
