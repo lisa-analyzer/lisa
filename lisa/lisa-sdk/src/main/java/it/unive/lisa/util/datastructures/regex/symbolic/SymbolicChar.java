10b377781e404e539cc92d1a2c3a19fca6325c70
lucaneg <luca.negrinii@gmail.com>
2022-12-21 18:22:51 +0100

    Adding symbolic strings for tarsis #232

---
+++ lisa/lisa-sdk/src/main/java/it/unive/lisa/util/datastructures/regex/symbolic/SymbolicChar.java
@@ -0,0 +1,70 @@
+package it.unive.lisa.util.datastructures.regex.symbolic;
+
+/**
+ * An symbolic character, that is, an object representing a single and possibly
+ * unknown character.
+ * 
+ * @author <a href="mailto:luca.negrini@unive.it">Luca Negrini</a>
+ */
+public class SymbolicChar {
+
+	private final char ch;
+
+	/**
+	 * Builds the symbolic character.
+	 * 
+	 * @param ch the underlying character
+	 */
+	public SymbolicChar(char ch) {
+		this.ch = ch;
+	}
+
+	/**
+	 * Converts this symbolic character to a normal character.
+	 * 
+	 * @return the character
+	 */
+	public char asChar() {
+		return ch;
+	}
+
+	/**
+	 * Yields {@code true} if and only if this symbolic character represent the
+	 * given concrete one.
+	 * 
+	 * @param ch the character
+	 * 
+	 * @return {@code true} if that condition holds
+	 */
+	public boolean is(char ch) {
+		return this.ch == ch;
+	}
+
+	@Override
+	public int hashCode() {
+		return Character.hashCode(ch);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+
+		if (obj == null)
+			return false;
+
+		if (getClass() != obj.getClass())
+			return false;
+
+		SymbolicChar other = (SymbolicChar) obj;
+		if (ch != other.ch)
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(ch);
+	}
+}
---
+++ lisa/lisa-sdk/src/main/java/it/unive/lisa/util/datastructures/regex/symbolic/UnknownSymbolicChar.java
@@ -0,0 +1,30 @@
+package it.unive.lisa.util.datastructures.regex.symbolic;
+
+import it.unive.lisa.util.datastructures.regex.TopAtom;
+
+/**
+ * An {@link SymbolicChar} representing an unknown character.
+ * 
+ * @author <a href="mailto:luca.negrini@unive.it">Luca Negrini</a>
+ */
+public class UnknownSymbolicChar extends SymbolicChar {
+
+	/**
+	 * The singleton instance.
+	 */
+	public static final UnknownSymbolicChar INSTANCE = new UnknownSymbolicChar();
+
+	private UnknownSymbolicChar() {
+		super(TopAtom.STRING.charAt(0));
+	}
+
+	@Override
+	public boolean is(char ch) {
+		return false;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return obj instanceof UnknownSymbolicChar;
+	}
+}
