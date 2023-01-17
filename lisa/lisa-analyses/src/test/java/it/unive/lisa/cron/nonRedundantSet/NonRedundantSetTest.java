52852748cada8464f3636faf64a63c614735ddbe
matte <matte@DESKTOP-7VA4V46>
2023-01-17 17:28:02 +0100

    added test for non redundant set of interval

---
+++ lisa/lisa-analyses/src/test/java/it/unive/lisa/cron/nonRedundantSet/NonRedundantSetTest.java
@@ -0,0 +1,29 @@
+package it.unive.lisa.cron.nonRedundantSet;
+
+import static it.unive.lisa.LiSAFactory.getDefaultFor;
+
+import it.unive.lisa.AnalysisSetupException;
+import it.unive.lisa.AnalysisTestExecutor;
+import it.unive.lisa.LiSAConfiguration;
+import it.unive.lisa.LiSAConfiguration.DescendingPhaseType;
+import it.unive.lisa.analysis.AbstractState;
+import it.unive.lisa.analysis.heap.HeapDomain;
+import it.unive.lisa.analysis.nonRedundantSet.NonRedundantPowersetOfInterval;
+import it.unive.lisa.analysis.nonrelational.value.TypeEnvironment;
+import it.unive.lisa.analysis.numeric.Interval;
+import it.unive.lisa.analysis.types.InferredTypes;
+import org.junit.Test;
+
+public class NonRedundantSetTest extends AnalysisTestExecutor {
+
+	@Test
+	public void testNonRedundantSetOfInerval() throws AnalysisSetupException {
+		LiSAConfiguration conf = new LiSAConfiguration();
+		conf.serializeResults = true;
+		conf.abstractState = getDefaultFor(AbstractState.class, getDefaultFor(HeapDomain.class), new NonRedundantPowersetOfInterval(),
+				new TypeEnvironment<>(new InferredTypes()));
+		conf.descendingPhaseType = DescendingPhaseType.GLB;
+		conf.descendingGlbThreshold = 5;
+		perform("descending-maxglb", "program.imp", conf);
+	}
+}
