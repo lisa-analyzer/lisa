7363aff50faba96fd62427c9c7649a571db37717
lucaneg <luca.negrinii@gmail.com>
2023-03-09 16:05:22 +0100

    Passing optimization options to fixpoints #248

--- lisa/lisa-sdk/src/main/java/it/unive/lisa/conf/FixpointConfiguration.java
+++ lisa/lisa-sdk/src/main/java/it/unive/lisa/conf/FixpointConfiguration.java
@@ -1,6 +1,9 @@
 package it.unive.lisa.conf;
 
+import java.util.function.Predicate;
+
 import it.unive.lisa.conf.LiSAConfiguration.DescendingPhaseType;
+import it.unive.lisa.program.cfg.statement.Statement;
 
 /**
  * An immutable configuration holding fixpoint-specific parameters.
@@ -24,6 +27,10 @@ public class FixpointConfiguration extends BaseConfiguration {
 	 */
 	public final DescendingPhaseType descendingPhaseType;
 
+	public final boolean optimize;
+
+	public final Predicate<Statement> hotspots;
+
 	/**
 	 * Builds the configuration.
 	 * 
@@ -33,5 +40,7 @@ public class FixpointConfiguration extends BaseConfiguration {
 		this.wideningThreshold = parent.wideningThreshold;
 		this.glbThreshold = parent.glbThreshold;
 		this.descendingPhaseType = parent.descendingPhaseType;
+		this.optimize = parent.optimize;
+		this.hotspots = parent.hotspots;
 	}
 }
