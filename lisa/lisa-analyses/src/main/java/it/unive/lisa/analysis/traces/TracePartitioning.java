1341bfe0d33954ccc742e1b6e1d26367aed36e84
lucaneg <luca.negrinii@gmail.com>
2023-03-17 19:22:53 +0100

    Fixing trace partitioning test #248

--- lisa/lisa-analyses/src/main/java/it/unive/lisa/analysis/traces/TracePartitioning.java
+++ lisa/lisa-analyses/src/main/java/it/unive/lisa/analysis/traces/TracePartitioning.java
@@ -171,7 +171,11 @@ public class TracePartitioning<A extends AbstractState<A, H, V, T>,
 				result.put(nextTrace, prev == null ? assume : assume.lub(prev));
 			}
 
-		return new TracePartitioning<>(lattice, result.isEmpty() ? null : result);
+		if (result.isEmpty())
+			// no traces pass the condition, so this branch is unreachable
+			return bottom();
+
+		return new TracePartitioning<>(lattice, result);
 	}
 
 	private static ExecutionTrace generateTraceFor(ExecutionTrace trace, ControlFlowStructure struct, ProgramPoint src,
