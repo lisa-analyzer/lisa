734de1d3de3b804be3b4a9cde3d963e6e8d4cd02
lucaneg <luca.negrinii@gmail.com>
2023-04-18 15:28:47 +0200

    Tests for FullStackToken #254

--- lisa/lisa-analyses/src/test/java/it/unive/lisa/cron/interprocedural/RecursionsTest.java
+++ lisa/lisa-analyses/src/test/java/it/unive/lisa/cron/interprocedural/RecursionsTest.java
@@ -2,6 +2,10 @@ package it.unive.lisa.cron.interprocedural;
 
 import static it.unive.lisa.LiSAFactory.getDefaultFor;
 
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
 import it.unive.lisa.AnalysisSetupException;
 import it.unive.lisa.AnalysisTestExecutor;
 import it.unive.lisa.CronConfiguration;
@@ -12,14 +16,33 @@ import it.unive.lisa.analysis.value.TypeDomain;
 import it.unive.lisa.interprocedural.callgraph.RTACallGraph;
 import it.unive.lisa.interprocedural.context.ContextBasedAnalysis;
 import it.unive.lisa.interprocedural.context.ContextInsensitiveToken;
+import it.unive.lisa.interprocedural.context.FullStackToken;
 import it.unive.lisa.interprocedural.context.KDepthToken;
 import it.unive.lisa.interprocedural.context.LastCallToken;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class RecursionsTest extends AnalysisTestExecutor {
+
+	@Test
+	public void testFibonacciFullStack() throws AnalysisSetupException {
+		CronConfiguration conf = new CronConfiguration();
+		conf.serializeResults = true;
+		conf.abstractState = getDefaultFor(AbstractState.class,
+				getDefaultFor(HeapDomain.class),
+				new Interval(),
+				getDefaultFor(TypeDomain.class));
+		conf.interproceduralAnalysis = new ContextBasedAnalysis<>(FullStackToken.getSingleton());
+		conf.callGraph = new RTACallGraph();
+		conf.testDir = "interprocedural";
+		conf.testSubDir = "fibonacci/full";
+		conf.programFile = "fibonacci.imp";
+		// as the test uses addition, there is a spurious x strcat y placed in
+		// the computed expressions in the first fixpoint round as the types of
+		// x and y cannot be determined (the state is bottom). This does not
+		// happen when unwinding the results, causing a difference.
+		conf.compareWithOptimization = false;
+		perform(conf);
+	}
 
 	@Test
 	public void testFibonacciKDepth() throws AnalysisSetupException {
@@ -85,6 +108,22 @@ public class RecursionsTest extends AnalysisTestExecutor {
 	}
 
 	@Test
+	public void testFactorialLoopFullStack() throws AnalysisSetupException {
+		CronConfiguration conf = new CronConfiguration();
+		conf.serializeResults = true;
+		conf.abstractState = getDefaultFor(AbstractState.class,
+				getDefaultFor(HeapDomain.class),
+				new Interval(),
+				getDefaultFor(TypeDomain.class));
+		conf.interproceduralAnalysis = new ContextBasedAnalysis<>(FullStackToken.getSingleton());
+		conf.callGraph = new RTACallGraph();
+		conf.testDir = "interprocedural";
+		conf.testSubDir = "factorialLoop/full";
+		conf.programFile = "factorialLoop.imp";
+		perform(conf);
+	}
+
+	@Test
 	public void testFactorialLoopKDepth() throws AnalysisSetupException {
 		CronConfiguration conf = new CronConfiguration();
 		conf.serializeResults = true;
@@ -129,6 +168,23 @@ public class RecursionsTest extends AnalysisTestExecutor {
 		conf.testDir = "interprocedural";
 		conf.testSubDir = "factorialLoop/insensitive";
 		conf.programFile = "factorialLoop.imp";
+		perform(conf);
+	}
+
+	@Test
+	public void testInfiniteRecursion2FullStack() throws AnalysisSetupException {
+		CronConfiguration conf = new CronConfiguration();
+		conf.serializeResults = true;
+		conf.abstractState = getDefaultFor(AbstractState.class,
+				getDefaultFor(HeapDomain.class),
+				new Interval(),
+				getDefaultFor(TypeDomain.class));
+		conf.interproceduralAnalysis = new ContextBasedAnalysis<>(FullStackToken.getSingleton());
+		conf.callGraph = new RTACallGraph();
+		conf.testDir = "interprocedural";
+		conf.testSubDir = "infiniteRecursion2/full";
+		conf.programFile = "infiniteRecursion2.imp";
+		// note: the result of this recursion is bottom as it never terminates
 		perform(conf);
 	}
 
@@ -179,6 +235,23 @@ public class RecursionsTest extends AnalysisTestExecutor {
 		conf.testDir = "interprocedural";
 		conf.testSubDir = "infiniteRecursion2/insensitive";
 		conf.programFile = "infiniteRecursion2.imp";
+		// note: the result of this recursion is bottom as it never terminates
+		perform(conf);
+	}
+
+	@Test
+	public void testInfiniteRecursion1FullStack() throws AnalysisSetupException {
+		CronConfiguration conf = new CronConfiguration();
+		conf.serializeResults = true;
+		conf.abstractState = getDefaultFor(AbstractState.class,
+				getDefaultFor(HeapDomain.class),
+				new Interval(),
+				getDefaultFor(TypeDomain.class));
+		conf.interproceduralAnalysis = new ContextBasedAnalysis<>(FullStackToken.getSingleton());
+		conf.callGraph = new RTACallGraph();
+		conf.testDir = "interprocedural";
+		conf.testSubDir = "infiniteRecursion1/full";
+		conf.programFile = "infiniteRecursion1.imp";
 		// note: the result of this recursion is bottom as it never terminates
 		perform(conf);
 	}
@@ -235,6 +308,22 @@ public class RecursionsTest extends AnalysisTestExecutor {
 	}
 
 	@Test
+	public void testFactorialFullStack() throws AnalysisSetupException {
+		CronConfiguration conf = new CronConfiguration();
+		conf.serializeResults = true;
+		conf.abstractState = getDefaultFor(AbstractState.class,
+				getDefaultFor(HeapDomain.class),
+				new Interval(),
+				getDefaultFor(TypeDomain.class));
+		conf.interproceduralAnalysis = new ContextBasedAnalysis<>(FullStackToken.getSingleton());
+		conf.callGraph = new RTACallGraph();
+		conf.testDir = "interprocedural";
+		conf.testSubDir = "factorial/full";
+		conf.programFile = "factorial.imp";
+		perform(conf);
+	}
+
+	@Test
 	public void testFactorialKDepth() throws AnalysisSetupException {
 		CronConfiguration conf = new CronConfiguration();
 		conf.serializeResults = true;
@@ -283,6 +372,22 @@ public class RecursionsTest extends AnalysisTestExecutor {
 	}
 
 	@Test
+	public void testFactorialInterleavedFullStack() throws AnalysisSetupException {
+		CronConfiguration conf = new CronConfiguration();
+		conf.serializeResults = true;
+		conf.abstractState = getDefaultFor(AbstractState.class,
+				getDefaultFor(HeapDomain.class),
+				new Interval(),
+				getDefaultFor(TypeDomain.class));
+		conf.interproceduralAnalysis = new ContextBasedAnalysis<>(FullStackToken.getSingleton());
+		conf.callGraph = new RTACallGraph();
+		conf.testDir = "interprocedural";
+		conf.testSubDir = "factorialInterleaved/full";
+		conf.programFile = "factorialInterleaved.imp";
+		perform(conf);
+	}
+
+	@Test
 	public void testFactorialInterleavedKDepth() throws AnalysisSetupException {
 		CronConfiguration conf = new CronConfiguration();
 		conf.serializeResults = true;
@@ -327,6 +432,22 @@ public class RecursionsTest extends AnalysisTestExecutor {
 		conf.testDir = "interprocedural";
 		conf.testSubDir = "factorialInterleaved/insensitive";
 		conf.programFile = "factorialInterleaved.imp";
+		perform(conf);
+	}
+
+	@Test
+	public void testTwoRecursionsFullStack() throws AnalysisSetupException {
+		CronConfiguration conf = new CronConfiguration();
+		conf.serializeResults = true;
+		conf.abstractState = getDefaultFor(AbstractState.class,
+				getDefaultFor(HeapDomain.class),
+				new Interval(),
+				getDefaultFor(TypeDomain.class));
+		conf.interproceduralAnalysis = new ContextBasedAnalysis<>(FullStackToken.getSingleton());
+		conf.callGraph = new RTACallGraph();
+		conf.testDir = "interprocedural";
+		conf.testSubDir = "twoRecursions/full";
+		conf.programFile = "twoRecursions.imp";
 		perform(conf);
 	}
 
