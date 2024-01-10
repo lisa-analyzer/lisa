cd351110a5c47f69a16f0ca025058ad5a53976cb
VincenzoArceri <vincenzoarceri.92@gmail.com>
2024-01-10 15:26:28 +0100

    Javadoc

--- lisa/lisa-analyses/src/main/java/it/unive/lisa/analysis/numeric/Pentagon.java
+++ lisa/lisa-analyses/src/main/java/it/unive/lisa/analysis/numeric/Pentagon.java
@@ -31,19 +31,19 @@ import java.util.function.Predicate;
 import org.apache.commons.collections4.CollectionUtils;
 
 /**
- * The pentagons abstract domain, a weakly relational numeric abstract domain.
- * This abstract domain captures properties of the form of x \in [a, b] &and; x
- * &lt; y. It is more precise than the well known interval domain, but it is
- * less precise than the octagon domain. It is implemented as a
+ * /** The pentagons abstract domain, a weakly relational numeric abstract
+ * domain. This abstract domain captures properties of the form of x \in [a, b]
+ * &and; x &lt; y. It is more precise than the well known interval domain, but
+ * it is less precise than the octagon domain. It is implemented as a
  * {@link ValueDomain}.
+ * 
+ * @author <a href="mailto:luca.negrini@unive.it">Luca Negrini</a>
+ * @author <a href="mailto:vincenzo.arceri@unipr.it">Vincenzo Arceri</a>
  * 
  * @see <a href=
  *          "https://www.sciencedirect.com/science/article/pii/S0167642309000719?ref=cra_js_challenge&fr=RR-1">Pentagons:
  *          A weakly relational abstract domain for the efficient validation of
  *          array accesses</a>
- * 
- * @author <a href="mailto:luca.negrini@unive.it">Luca Negrini</a>
- * @author <a href="mailto:vincenzo.arceri@unipr.it">Vincenzo Arceri</a>
  */
 public class Pentagon implements ValueDomain<Pentagon>, BaseLattice<Pentagon> {
 
