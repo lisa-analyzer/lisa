d7a6b34a079965204f756319adbd2e063d2b27de
phiash <sofiapresotto98@gmail.com>
2024-02-08 12:57:50 +0100

    assign method structure

--- lisa/lisa-analyses/src/main/java/it/unive/lisa/analysis/Stability.java
+++ lisa/lisa-analyses/src/main/java/it/unive/lisa/analysis/Stability.java
@@ -1,11 +1,22 @@
 package it.unive.lisa.analysis;
 
-import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
-import it.unive.lisa.analysis.numeric.Sign;
-import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.analysis.lattices.Satisfiability;
+import it.unive.lisa.analysis.nonrelational.value.ValueEnvironment;
+import it.unive.lisa.analysis.numeric.Interval;
+import it.unive.lisa.analysis.value.ValueDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.*;
+import it.unive.lisa.symbolic.value.operator.AdditionOperator;
+import it.unive.lisa.symbolic.value.operator.DivisionOperator;
+import it.unive.lisa.symbolic.value.operator.MultiplicationOperator;
+import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
 
-public class Stability implements BaseNonRelationalValueDomain<Stability> {
+import java.util.function.Predicate;
+
+public class Stability implements ValueDomain<Stability> {
 
     /**
      * The abstract top element.
@@ -49,18 +60,26 @@ public class Stability implements BaseNonRelationalValueDomain<Stability> {
 
     private final byte stability;
 
+    private final ValueEnvironment<Interval> intervals;
+
     public Stability(byte stability){
         this.stability = stability;
+        this.intervals = new ValueEnvironment<>(new Interval()).top();
+    }
+
+    public Stability(byte stability, ValueEnvironment<Interval> intervals) {
+        this.stability = stability;
+        this.intervals = intervals;
     }
 
     @Override
-    public Stability lubAux(Stability other) throws SemanticException {
-        return TOP;
-    }
-
-    @Override
-    public boolean lessOrEqualAux(Stability other) throws SemanticException {
+    public boolean lessOrEqual(Stability other) throws SemanticException {
         return false;
+    }
+
+    @Override
+    public Stability lub(Stability other) throws SemanticException {
+        return null;
     }
 
     @Override
@@ -74,26 +93,161 @@ public class Stability implements BaseNonRelationalValueDomain<Stability> {
     }
 
     @Override
+    public Stability pushScope(ScopeToken token) throws SemanticException {
+        return null;
+    }
+
+    @Override
+    public Stability popScope(ScopeToken token) throws SemanticException {
+        return null;
+    }
+
+    private boolean queryToAux(String expression, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        //return intervals.satisfies(expression, pp, oracle) == Satisfiability.SATISFIED;
+        return false;
+    }
+
+    /**
+     * Compares values of id and the result of expression in the auxiliary abstract domain
+     * @param id
+     * @param expression
+     * @param pp
+     * @param oracle
+     * @return  STABLE iff id == expression
+     *          [...]
+     */
+    private Stability auxCompareAfterAssignment(Identifier id, ValueExpression expression, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        if (queryToAux("x > expr", pp, oracle)) return DEC;
+        else if (queryToAux("x == expr", pp, oracle)) return STABLE;
+        else if (queryToAux("x < expr", pp, oracle)) return INC;
+        else if (queryToAux("x >= expr", pp, oracle)) return NON_INC;
+        else if (queryToAux("x <= expr", pp, oracle)) return NON_DEC;
+        else if (queryToAux("x != expr", pp, oracle)) return NON_STABLE;
+        else return TOP;
+    }
+
+
+
+    // TO DO: fix the queries
+    @Override
+    public Stability assign(Identifier id, ValueExpression expression, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+
+        if (expression instanceof UnaryExpression) {
+            UnaryExpression ue = (UnaryExpression) expression;
+            if (ue.getOperator() instanceof NumericNegation)
+                return auxCompareAfterAssignment(id, expression, pp, oracle);
+
+        } else if (expression instanceof BinaryExpression) {
+            BinaryExpression be = (BinaryExpression) expression;
+            BinaryOperator op = be.getOperator();
+            if (op instanceof AdditionOperator
+                    || id instanceof SubtractionOperator
+                    || id instanceof MultiplicationOperator
+                    || id instanceof DivisionOperator)
+                return auxCompareAfterAssignment(id, expression, pp, oracle);
+        }
+        return TOP;
+    }
+                /*
+        if (expression instanceof UnaryExpression){
+            UnaryExpression ue = (UnaryExpression) expression;
+
+            if (ue.getOperator() instanceof NumericNegation) {
+                // "x = - x"
+                if (id.equals(ue.getExpression())) {
+
+                    //return auxCompare(id, 0, pp, oracle);
+
+                    // x -> [0, 0]
+                    if (queryToAux("id == 0", pp, oracle))
+                        return STABLE;
+
+                        // x -> [a, b] with a > 0
+                    else if (queryToAux("id > 0", pp, oracle))
+                        return DEC;
+
+                        // x -> [a, b] with b < 0
+                    else if (queryToAux("id < 0", pp, oracle))
+                        return INC;
+
+                        // x -> [a, b] with a == 0 (b != 0)
+                    else if (queryToAux("id >= 0", pp, oracle))
+                        return NON_INC;
+
+                        // x -> [a, b] with b == 0 (a != 0)
+                    else if (queryToAux("id <= 0", pp, oracle))
+                        return NON_DEC;
+
+                        // x -> [a, b] with a > 0 or b < 0
+                    else if (queryToAux("id != 0", pp, oracle))
+                        return NON_STABLE;
+
+                    else return TOP;
+
+                }
+                // "x = - expr"
+                else return auxCompareAfterAssignment(id, expression, pp, oracle);
+            }
+
+            // Q: direttamente TOP?
+            else return TOP;
+        }
+
+        else if (expression instanceof BinaryExpression){
+            BinaryExpression be = (BinaryExpression) expression;
+            BinaryOperator op = be.getOperator();
+            boolean isLeft = id.equals(be.getLeft());
+            boolean isRight = id.equals(be.getRight());
+
+            // x = a + b
+            if(op instanceof AdditionOperator){
+                if (isLeft){
+                    if (isRight) return auxCompareAfterAssignment(id, expression, pp, oracle);
+                    }
+                }
+
+
+            }
+
+            return TOP;
+        }
+
+        else return TOP;
+    }
+    */
+
+    @Override
+    public Stability smallStepSemantics(ValueExpression expression, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        return null;
+    }
+
+    @Override
+    public Stability assume(ValueExpression expression, ProgramPoint src, ProgramPoint dest, SemanticOracle oracle) throws SemanticException {
+        return null;
+    }
+
+    @Override
+    public boolean knowsIdentifier(Identifier id) {
+        return false;
+    }
+
+    @Override
+    public Stability forgetIdentifier(Identifier id) throws SemanticException {
+        return null;
+    }
+
+    @Override
+    public Stability forgetIdentifiersIf(Predicate<Identifier> test) throws SemanticException {
+        return null;
+    }
+
+    @Override
+    public Satisfiability satisfies(ValueExpression expression, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        return null;
+    }
+
+    @Override
     public StructuredRepresentation representation() {
-        if (isBottom())
-            return Lattice.bottomRepresentation();
-        if (isTop())
-            return Lattice.topRepresentation();
-
-        String repr;
-        if (this == INC)
-            repr = "increasing";
-        else if (this == DEC)
-            repr = "decreasing";
-        else if (this == STABLE)
-            repr = "stable";
-        else if (this == NON_INC)
-            repr = "non-increasing";
-        else if (this == NON_DEC)
-            repr = "non-decreasing";
-        else
-            repr = "not stable";
-
-        return new StringRepresentation(repr);
+        return null;
     }
 }
