69f11580b10360e05a5bec0f84b0ffa01c71e499
Luca Negrini <luca.negrinii@gmail.com>
2025-11-06 10:46:10 +0100

    Missing javadoc

--- lisa/lisa-analyses/src/main/java/it/unive/lisa/lattices/heap/allocations/NullAllocationSite.java
+++ lisa/lisa-analyses/src/main/java/it/unive/lisa/lattices/heap/allocations/NullAllocationSite.java
@@ -3,10 +3,18 @@ package it.unive.lisa.lattices.heap.allocations;
 import it.unive.lisa.program.SyntheticLocation;
 import it.unive.lisa.type.NullType;
 
+/**
+ * A singleton allocation site representing a uninitialized/null location.
+ * 
+ * @author <a href="mailto:luca.negrini@unive.it">Luca Negrini</a>
+ */
 public class NullAllocationSite
 		extends
 		HeapAllocationSite {
 
+	/**
+	 * The singleton instance of this allocation site.
+	 */
 	public static NullAllocationSite INSTANCE = new NullAllocationSite();
 
 	private NullAllocationSite() {
