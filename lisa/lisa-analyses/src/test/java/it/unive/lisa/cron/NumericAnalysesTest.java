2a30dec32c6e6f91682fd4137c345f38fc625e44
Luca Negrini <luca.negrinii@gmail.com>
2025-12-22 09:01:12 +0100

    adding history abstraction #334

--- lisa/lisa-analyses/src/test/java/it/unive/lisa/cron/NumericAnalysesTest.java
+++ lisa/lisa-analyses/src/test/java/it/unive/lisa/cron/NumericAnalysesTest.java
@@ -1,12 +1,14 @@
 package it.unive.lisa.cron;
 
 import it.unive.lisa.DefaultConfiguration;
+import it.unive.lisa.analysis.HistoryDomain;
 import it.unive.lisa.analysis.numeric.IntegerConstantPropagation;
 import it.unive.lisa.analysis.numeric.Interval;
 import it.unive.lisa.analysis.numeric.NonRedundantIntervals;
 import it.unive.lisa.analysis.numeric.Parity;
 import it.unive.lisa.analysis.numeric.Pentagon;
 import it.unive.lisa.analysis.numeric.Sign;
+import it.unive.lisa.outputs.HtmlResults;
 import it.unive.lisa.outputs.JSONResults;
 import it.unive.lisa.program.cfg.fixpoints.forward.ForwardDescendingGLBFixpoint;
 import org.junit.Test;
@@ -54,6 +56,25 @@ public class NumericAnalysesTest
 		conf.testDir = "numeric";
 		conf.testSubDir = "interval";
 		conf.programFile = "numeric.imp";
+		perform(conf);
+	}
+
+	@Test
+	public void testIntervalWithHistory() {
+		CronConfiguration conf = new CronConfiguration();
+		conf.outputs.add(new JSONResults<>());
+		conf.analysis = new HistoryDomain<>(
+				DefaultConfiguration.simpleDomain(
+						DefaultConfiguration.defaultHeapDomain(),
+						new Interval(),
+						DefaultConfiguration.defaultTypeDomain()));
+		conf.testDir = "numeric";
+		conf.testSubDir = "interval-history";
+		conf.programFile = "numeric.imp";
+		// the unwinding is inherently different: since the loop guard has
+		// already the final widened approximation, all history from loop
+		// iterations is lost
+		conf.compareWithOptimization = false;
 		perform(conf);
 	}
 
