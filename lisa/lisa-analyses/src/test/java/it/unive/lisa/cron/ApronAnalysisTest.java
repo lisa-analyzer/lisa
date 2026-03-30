e94019efdee16ee30af94edef62604c740b73aa0
Rocco <emsicoding@gmail.com>
2026-03-30 23:09:17 +0200

    indent ApronAnalysisTest class and prepare Octagon test

--- lisa/lisa-analyses/src/test/java/it/unive/lisa/cron/ApronAnalysisTest.java
+++ lisa/lisa-analyses/src/test/java/it/unive/lisa/cron/ApronAnalysisTest.java
@@ -2,6 +2,7 @@ package it.unive.lisa.cron;
 
 import it.unive.lisa.DefaultConfiguration;
 import it.unive.lisa.analysis.apron.Apron;
+import it.unive.lisa.outputs.HtmlResults;
 import it.unive.lisa.outputs.JSONResults;
 import org.junit.jupiter.api.Test;
 
@@ -10,9 +11,11 @@ public class ApronAnalysisTest
 		IMPCronExecutor {
 
 	@Test
-	public void testApron() {
+	public void testApronBox() {
 
 		Apron.loadLibrary();
+
+		Apron.setManager(Apron.ApronDomain.Box);
 
 		CronConfiguration conf = new CronConfiguration();
 		conf.outputs.add(new JSONResults<>());
@@ -22,9 +25,27 @@ public class ApronAnalysisTest
 				DefaultConfiguration.defaultHeapDomain(),
 				new Apron(),
 				DefaultConfiguration.defaultTypeDomain());
-		conf.testDir = "numeric/apron";
-		conf.programFile = "numeric_apron.imp";
+		conf.testDir = "numeric/apron-box";
+		conf.programFile = "box.imp";
 		perform(conf);
 	}
 
+	@Test
+	public void testApronOctagon() {
+		Apron.loadLibrary();
+
+		Apron.setManager(Apron.ApronDomain.Octagon);
+
+		CronConfiguration conf = new CronConfiguration();
+		conf.outputs.add(new JSONResults<>());
+		conf.outputs.add(new HtmlResults<>(true));
+		// conf.forceUpdate = true;
+		conf.analysis = DefaultConfiguration.simpleDomain(
+				DefaultConfiguration.defaultHeapDomain(),
+				new Apron(),
+				DefaultConfiguration.defaultTypeDomain());
+		conf.testDir = "numeric/apron-octagon";
+		conf.programFile = "octagon.imp";
+		perform(conf);
+	}
 }
